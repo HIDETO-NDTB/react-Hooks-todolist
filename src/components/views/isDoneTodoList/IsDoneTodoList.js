714e7691bdf6a5719251108b108a2c3484815a04
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2021-03-13 11:53:22 +0900

    implement search fanction with isDoneTodoList

--- src/components/views/isDoneTodoList/IsDoneTodoList.js
+++ src/components/views/isDoneTodoList/IsDoneTodoList.js
@@ -5,14 +5,22 @@ import IsDoneTodo from "./IsDoneTodo";
 const IsDoneTodoList = () => {
   const { state } = useContext(AppContext);
 
-  state.isDoneTodos.sort((a, b) => {
+  const filterCollection = (todo) => {
+    const regExp = new RegExp("^" + state.searchText, "i");
+    return todo.name.match(regExp);
+  };
+  const data = state.searchText
+    ? state.isDoneTodos.filter(filterCollection)
+    : state.isDoneTodos;
+
+  data.sort((a, b) => {
     return a.orderNo - b.orderNo;
   });
 
   return (
     <div className="todos-list">
       <h4 className="section-title">処理済みTodo 一覧</h4>
-      {state.isDoneTodos.map((todo) => (
+      {data.map((todo) => (
         <div className="todo-list" key={todo.id}>
           <IsDoneTodo todo={todo} />
         </div>
--- src/components/views/todoList/TodoList.js
+++ src/components/views/todoList/TodoList.js
@@ -9,12 +9,10 @@ const TodoList = () => {
     const regExp = new RegExp("^" + state.searchText, "i");
     return todo.name.match(regExp);
   };
-
   // filterの引数に関数ilterCollectionを渡し、state.todosを絞り込む
   const data = state.searchText
     ? state.todos.filter(filterCollection)
     : state.todos;
-
   // orderNoが小さい順番に並べ替え
   data.sort(function (a, b) {
     return a.orderNo - b.orderNo;
