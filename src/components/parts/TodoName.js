b5b2a77b14352daa63517212c6c728a479c5cabe
HIDETO-NDTB <neckwisteria@yahoo.co.jp>
2021-03-13 10:12:02 +0900

    create TodoName component

---
+++ src/components/parts/TodoName.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext } from "react";
+import AppContext from "../../contexts/AppContext";
+import { TOGGLE_EDITMODE, UPDATE_TODO, CANCEL_UPDATE } from "../../actions";
+
+const TodoName = ({ todo }) => {
+  const [updatedName, setUpdatedName] = useState("");
+  const { dispatch } = useContext(AppContext);
+
+  const handleToggleEditMode = (id) => {
+    dispatch({
+      type: TOGGLE_EDITMODE,
+      id,
+    });
+  };
+  const handleUpdateTodo = (id) => {
+    dispatch({
+      type: UPDATE_TODO,
+      id,
+      name: updatedName,
+    });
+    todo.editMode = false;
+    setUpdatedName("");
+  };
+  const handleCancelUpdate = (id) => {
+    dispatch({
+      type: CANCEL_UPDATE,
+      id,
+    });
+  };
+
+  return todo.editMode ? (
+    <>
+      <input
+        type="text"
+        className="todo-input"
+        onChange={(e) => setUpdatedName(e.target.value)}
+        defaultValue={todo.name}
+      />
+      <button
+        className="btn update-todo-button"
+        onClick={() => handleUpdateTodo(todo.id)}
+        disabled={!updatedName.length}
+      >
+        todo修正
+      </button>
+      <button
+        className="btn cancel-update-button"
+        onClick={() => handleCancelUpdate(todo.id)}
+      >
+        キャンセル
+      </button>
+    </>
+  ) : (
+    <p className="todo-text" onClick={() => handleToggleEditMode(todo.id)}>
+      {todo.name}
+    </p>
+  );
+};
+
+export default TodoName;
--- src/components/todoList/Todo.js
+++ src/components/todoList/Todo.js
@@ -1,77 +1,20 @@
-import React, { useState, useContext } from "react";
-import { CANCEL_UPDATE, TOGGLE_EDITMODE, UPDATE_TODO } from "../../actions";
-import AppContext from "../../contexts/AppContext";
+import React from "react";
 import InputOrder from "../parts/InputOrder";
 import ExpectDate from "../parts/ExpectDate";
 import CheckBox from "../parts/CheckBox";
 import Trash from "../parts/Trash";
+import TodoName from "../parts/TodoName";
 
 const Todo = ({ todo }) => {
-  const [updatedName, setUpdatedName] = useState("");
-  const { dispatch } = useContext(AppContext);
-
-  const handleToggleEditMode = (id) => {
-    dispatch({
-      type: TOGGLE_EDITMODE,
-      id,
-    });
-  };
-  const handleUpdateTodo = (id) => {
-    dispatch({
-      type: UPDATE_TODO,
-      id,
-      name: updatedName,
-    });
-    todo.editMode = false;
-    setUpdatedName("");
-  };
-  const handleCancelUpdate = (id) => {
-    dispatch({
-      type: CANCEL_UPDATE,
-      id,
-    });
-  };
-
   return (
     <>
       <div className={todo.isDone ? "todoList todo-isDone" : "todoList"}>
         <CheckBox todo={todo} />
-        {todo.editMode ? (
-          <input
-            type="text"
-            className="todo-input"
-            onChange={(e) => setUpdatedName(e.target.value)}
-            defaultValue={todo.name}
-          />
-        ) : (
-          <p
-            className="todo-text"
-            onClick={() => handleToggleEditMode(todo.id)}
-          >
-            {todo.name}
-          </p>
-        )}
+        <TodoName todo={todo} />
         <InputOrder todo={todo} />
         <Trash todo={todo} />
         <ExpectDate todo={todo} />
       </div>
-      {todo.editMode && (
-        <>
-          <button
-            className="btn update-todo-button"
-            onClick={() => handleUpdateTodo(todo.id)}
-            disabled={!updatedName.length}
-          >
-            todo修正
-          </button>
-          <button
-            className="btn cancel-update-button"
-            onClick={() => handleCancelUpdate(todo.id)}
-          >
-            キャンセル
-          </button>
-        </>
-      )}
     </>
   );
 };
